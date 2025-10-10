c444ace078f4e9db782bcdd9b882bfbc8806d52e
Adrian-Koh <adriankoh216@yahoo.com>
2025-10-10 18:21:15 +0800

    Add accept/send status to follow buttons

--- frontend/src/api/follow.js
+++ frontend/src/api/follow.js
@@ -17,3 +17,36 @@ export const toggleFollowUser = async (userId, follow) => {
     throw new Error(parsed.message);
   }
 };
+
+export const acceptFollowRequest = async (userId) => {
+  const response = await fetch(BACKEND_DOMAIN + "/accept/" + userId, {
+    headers: getTokenHeader(),
+    method: "POST",
+  });
+
+  const parsed = await response.json();
+  console.log(
+    "parsed response from accept follow request: " + JSON.stringify(parsed)
+  );
+
+  if (!response.ok) {
+    throw new Error(parsed.message);
+  }
+};
+
+export const getFollowRequests = async () => {
+  const response = await fetch(BACKEND_DOMAIN + "/requests", {
+    headers: getTokenHeader(),
+  });
+
+  const parsed = await response.json();
+  console.log(
+    "parsed response from get follow request: " + JSON.stringify(parsed)
+  );
+
+  if (!response.ok) {
+    throw new Error(parsed.message);
+  }
+
+  return { received: parsed.received, sent: parsed.sent };
+};
--- frontend/src/components/AllUsers/AllUsers.jsx
+++ frontend/src/components/AllUsers/AllUsers.jsx
@@ -1,49 +1,74 @@
 import { useContext, useEffect, useState } from "react";
 import styles from "./AllUsers.module.css";
 import { getAllUsers } from "../../api/users";
-import { toggleFollowUser } from "../../api/follow";
+import {
+  toggleFollowUser,
+  getFollowRequests,
+  acceptFollowRequest,
+} from "../../api/follow";
 import { HomeContext } from "../../pages/Home/Home";
 
 const AllUsers = () => {
   const [users, setUsers] = useState([]);
+  const [requestsSent, setRequestsSent] = useState([]);
+  const [requestsReceived, setRequestsReceived] = useState([]);
   const { following, fetchFollowing } = useContext(HomeContext);
 
+  const fetchUsersCb = async () => {
+    const fetchedUsers = await getAllUsers();
+    setUsers(fetchedUsers);
+  };
+  const fetchFollowRequestsCb = async () => {
+    const { received, sent } = await getFollowRequests();
+    if (received && received.length > 0)
+      setRequestsReceived(received.map((req) => req.requesterId));
+    if (sent && sent.length > 0)
+      setRequestsSent(sent.map((res) => res.responderId));
+  };
   useEffect(() => {
-    const fetchUsersCb = async () => {
-      const fetchedUsers = await getAllUsers();
-      setUsers(fetchedUsers);
-    };
     fetchUsersCb();
     fetchFollowing();
+    fetchFollowRequestsCb();
   }, []);
 
   async function handleFollowClick(userId, follow) {
     await toggleFollowUser(userId, follow);
     fetchFollowing();
+    fetchFollowRequestsCb();
+  }
+
+  async function handleFollowRequest(userId, accept) {
+    if (accept) await acceptFollowRequest(userId);
+    fetchFollowRequestsCb();
   }
 
   return (
     <div className={styles.container}>
       <h2>All Users</h2>
       <div className={styles.allUsers}>
-        {users.map((user) => (
-          <div className={styles.user} key={user.id}>
+        {users.map((otherUser) => (
+          <div className={styles.user} key={otherUser.id}>
             <img
-              src={user.avatarUrl ? user.avatarUrl : "/face-man-profile.svg"}
+              src={
+                otherUser.avatarUrl
+                  ? otherUser.avatarUrl
+                  : "/face-man-profile.svg"
+              }
               alt="profile pic"
               className={
-                user.avatarUrl
+                otherUser.avatarUrl
                   ? styles.profilePic
                   : `${styles.profilePic} genericPic`
               }
             ></img>
-            <div className={styles.displayName}>{user.displayName}</div>
-            <div className={styles.email}>{user.email}</div>
-            {following.filter((follow) => follow.followingId === user.id)
+            <div className={styles.displayName}>{otherUser.displayName}</div>
+            <div className={styles.email}>{otherUser.email}</div>
+            {following.filter((follow) => follow.followingId === otherUser.id)
               .length > 0 ? (
+              // if logged in user is already following this other user
               <button
                 className={styles.followingBtn}
-                onClick={() => handleFollowClick(user.id, false)}
+                onClick={() => handleFollowClick(otherUser.id, false)}
               >
                 <img
                   src="/check.svg"
@@ -53,16 +78,47 @@ const AllUsers = () => {
                 Following
               </button>
             ) : (
+              // if logged in user is NOT following this other user
               <button
                 className={styles.followBtn}
-                onClick={() => handleFollowClick(user.id, true)}
+                onClick={() => {
+                  if (requestsReceived.includes(otherUser.id))
+                    handleFollowRequest(otherUser.id, true);
+                  else if (requestsSent.includes(otherUser.id))
+                    handleFollowRequest(otherUser.id, false);
+                  else handleFollowClick(otherUser.id, true);
+                }}
               >
-                <img
-                  src="/plus.svg"
-                  alt="follow"
-                  className={styles.followIcon}
-                />
-                Follow
+                {requestsReceived.includes(otherUser.id) ? (
+                  // if other user sent a follow request to logged in user
+                  <>
+                    <img
+                      src="/check.svg"
+                      alt="accept"
+                      className={styles.followIcon} // todo: use other class name
+                    />
+                    Accept follow request
+                  </>
+                ) : requestsSent.includes(otherUser.id) ? (
+                  // if logged in user sent a follow request to other user
+                  <>
+                    <img
+                      src="/clock-outline.svg"
+                      alt="pending"
+                      className={styles.followIcon} // todo: use other class name
+                    />
+                    Follow request sent
+                  </>
+                ) : (
+                  <>
+                    <img
+                      src="/plus.svg"
+                      alt="follow"
+                      className={styles.followIcon}
+                    />
+                    Follow
+                  </>
+                )}
               </button>
             )}
           </div>
